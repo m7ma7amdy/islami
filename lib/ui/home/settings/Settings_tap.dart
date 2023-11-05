c2ccd7a270a11c4893c550d795b772e592b781d9
Hamdyy <fcts021@gmail.com>
2023-11-05 04:30:14 +0200

    +provider

--- lib/ui/home/settings/Settings_tap.dart
+++ lib/ui/home/settings/Settings_tap.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/provaider/Settinges_Provider.dart';
 import 'package:islami/ui/home/settings/LanguageBottomSheet.dart';
 import 'package:islami/ui/home/settings/ThemBottomSheet.dart';
+import 'package:provider/provider.dart';
 
 class SettingsTap extends StatefulWidget {
   @override
@@ -10,12 +13,13 @@ class SettingsTap extends StatefulWidget {
 class _SettingsTapState extends State<SettingsTap> {
   @override
   Widget build(BuildContext context) {
+    var settingsProvider = Provider.of<SettingsProvider>(context);
     return Container(
       padding: EdgeInsets.symmetric(vertical: 64, horizontal: 18),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Them'),
+          Text(AppLocalizations.of(context)!.theme),
           InkWell(
             onTap: () {
               showThemeBottomSheet();
@@ -29,14 +33,16 @@ class _SettingsTapState extends State<SettingsTap> {
                     border: Border.all(
                         color: Theme.of(context).dividerColor, width: 1)),
                 child: Text(
-                  'Light',
+                  settingsProvider.isDarkEnabled()
+                      ? AppLocalizations.of(context)!.dark
+                      : AppLocalizations.of(context)!.light,
                   style: Theme.of(context).textTheme.titleMedium,
                 )),
           ),
           SizedBox(
             height: 18,
           ),
-          Text('Language'),
+          Text(AppLocalizations.of(context)!.language),
           InkWell(
             onTap: () {
               showLanguageBottomSheet();
@@ -50,7 +56,9 @@ class _SettingsTapState extends State<SettingsTap> {
                     border: Border.all(
                         color: Theme.of(context).dividerColor, width: 1)),
                 child: Text(
-                  'English',
+                  settingsProvider.currentLocale == 'en'
+                      ? 'English'
+                      : 'العربيه',
                   style: Theme.of(context).textTheme.titleMedium,
                 )),
           )
--- lib/ui/home/settings/ThemBottomSheet.dart
+++ lib/ui/home/settings/ThemBottomSheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami/provaider/Settinges_Provider.dart';
+import 'package:provider/provider.dart';
 
 class ThemBottomSheet extends StatefulWidget {
   @override
@@ -8,12 +11,28 @@ class ThemBottomSheet extends StatefulWidget {
 class _ThemBottomSheetState extends State<ThemBottomSheet> {
   @override
   Widget build(BuildContext context) {
+    var settingsProvide = Provider.of<SettingsProvider>(context);
     return Container(
       padding: EdgeInsets.all(12),
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [getSelectedItem('Light'), unSelectedItem('Dark')],
+        children: [
+          InkWell(
+              onTap: () {
+                settingsProvide.changeTheme(ThemeMode.light);
+              },
+              child: settingsProvide.isDarkEnabled()
+                  ? getUnSelectedItem(AppLocalizations.of(context)!.light)
+                  : getSelectedItem(AppLocalizations.of(context)!.light)),
+          InkWell(
+              onTap: () {
+                settingsProvide.changeTheme(ThemeMode.dark);
+              },
+              child: settingsProvide.isDarkEnabled()
+                  ? getSelectedItem(AppLocalizations.of(context)!.dark)
+                  : getUnSelectedItem(AppLocalizations.of(context)!.dark))
+        ],
       ),
     );
   }
@@ -35,7 +54,11 @@ class _ThemBottomSheetState extends State<ThemBottomSheet> {
     );
   }
 
-  Widget unSelectedItem(String text) {
-    return Text(text, style: Theme.of(context).textTheme.titleMedium);
+  Widget getUnSelectedItem(String text) {
+    return Row(
+      children: [
+        Text(text, style: Theme.of(context).textTheme.titleMedium),
+      ],
+    );
   }
 }
